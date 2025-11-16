930ddd8881d9a8df4314f17369422d357d936d6e
Stanislav-Kudelkin <kudelkinstas128@gmail.com>
2025-11-16 15:35:48 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  // const [value, setValue] = useLocalStorage('inputValue');
+
+  return (
+    <>
+      <p>
+        <label style={{ marginRight: '10px' }}>Сохраненное значение:</label>
+        {/* {value} */}
+      </p>
+      <p>
+        <input />
+      </p>
+    </>
+  );
+}
+
+export default App;
